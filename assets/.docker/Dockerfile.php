06b7fa3ab57bff11de54553e5266dc6c8f00f982
Guy Owen <guy.owen@dpc.vic.gov.au>
2020-12-11 13:33:03 +1100

    Unset arg assigment and assigned default.

--- assets/.docker/Dockerfile.php
+++ assets/.docker/Dockerfile.php
@@ -1,8 +1,8 @@
 # @see https://github.com/dpc-sdp/bay/blob/master/bay/images/Dockerfile.php
-ARG BAY_IMAGE_VERSION=latest
+ARG BAY_IMAGE_VERSION
 ARG CLI_IMAGE
 FROM ${CLI_IMAGE:-cli} as cli
 
-FROM singledigital/bay-php:${BAY_IMAGE_VERSION}
+FROM singledigital/bay-php:${BAY_IMAGE_VERSION:-latest}
 
 COPY --from=cli /app /app
